23ab36afd1da88cceeb85a3fa8b00693af5b8c4b
LUKASIEWICZ\serhii.baraban <serhii.baraban@pit.lukasiewicz.gov.pl>
2022-12-08 11:59:54 +0100

    Flutter UI app - native for Windows, macOS

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = 'abbbbdcfd670dc9276ab';
+const githubClientSecret = 'dff36a6a3149d592f8ffc159be6d2d623797e77f';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
